4807044beceb9750a78ee3990b63b43a3087500d
Alexandre Costa <alexandremcost@gmail.com>
2021-05-04 15:38:26 -0300

    prepare to deploy on render.com

--- config/releases.exs
+++ config/releases.exs
@@ -2,7 +2,10 @@
 # from environment variables. You can also hardcode secrets,
 # although such is generally not recommended and you have to
 # remember to add this file to your .gitignore.
-use Mix.Config
+
+# render.com
+# use Mix.Config
+import Config
 
 database_url =
   System.get_env("DATABASE_URL") ||
@@ -35,7 +38,7 @@ config :mariel, MarielWeb.Endpoint,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :mariel, MarielWeb.Endpoint, server: true
+config :mariel, MarielWeb.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
